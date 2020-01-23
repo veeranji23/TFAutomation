adbc0bf4f6a2849739ead380298d423af288ff1b
veeranji23 <48329756+veeranji23@users.noreply.github.com>
2020-01-23 23:48:53 +0530

    simple code

--- main.tf
+++ main.tf
@@ -13,35 +13,10 @@ provider "aws" {
 }
 
 # Create EC2 instance
-resource "aws_instance" "default" {
-  ami                    = var.ami
-  count                  = var.instance_count
-  key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.default.id]
-  source_dest_check      = false
-  instance_type          = var.instance_type
-
+resource "aws_instance" "example" {
+  ami           = "ami-00068cd7555f543d5"
+  instance_type = "t2.micro"
   tags = {
-    Name = "terraform-default"
-  }
+    Name = "TerraformDemo"
 }
-
-# Create Security Group for EC2
-resource "aws_security_group" "default" {
-  name = "terraform-default-sg"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
 }
