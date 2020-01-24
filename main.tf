a0e1ea9eebc1733c085a9b6469cc674ec6cf6e95
veeranji23 <48329756+veeranji23@users.noreply.github.com>
2020-01-24 11:48:03 +0530

    modifications -1

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-alex"
+    bucket = "terraform-bucket-veeratest2323232323"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
