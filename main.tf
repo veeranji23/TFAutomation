55a459f4921aa19f479844579013615d4e3be32a
veeranji23 <48329756+veeranji23@users.noreply.github.com>
2020-02-06 11:05:29 +0530

    Added VPC and Subnet

--- main.tf
+++ main.tf
@@ -57,3 +57,20 @@ resource "aws_s3_bucket" "b" {
     enabled = true
   }
 }
+resource "aws_vpc" "vpc" {
+  cidr_block       = "190.160.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "TerraformVPC"
+    Location = "Hyderabad"
+  }
+}
+resource "aws_subnet" "subnet1" {
+  vpc_id     = "${aws_vpc.vpc.id}"
+  cidr_block = "190.160.0.0/16"
+
+  tags = {
+    Name = "Subnet1"
+  }
+}
