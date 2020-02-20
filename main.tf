1408cae392e775abf34d01bc238781373145f3c3
veeranji23 <48329756+veeranji23@users.noreply.github.com>
2020-02-20 10:48:17 +0530

    Commit 2

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "default" {
   instance_type          = var.instance_type
 
   tags = {
-    Name = "TFA-Integration with UI"
+    Name = "TFA-Jenkins-EnvProviosion"
   }
 }
 
